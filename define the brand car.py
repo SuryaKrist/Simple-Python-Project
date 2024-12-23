b402ce3dafdd487d8388ffcbdd8f60517531800f
Surya Kristanto <156643336+SuryaKrist@users.noreply.github.com>
2024-12-23 07:32:50 +0700

    Add files via upload

---
+++ define the brand car.py
@@ -0,0 +1,10 @@
+class car:
+    def __init__(self, color, brand, speed):
+        self.color = color
+        self.brand = brand
+        self.speed = speed
+car1 = car("Red", "BMW", 19000)
+print(car1.color)
+print(car1.brand)
+print(car1.speed)
+
